a73fdae60eba7f021148e1c1e7f5be6f9f77733e
mohamedsayedahmd <mohamedsayedahmd@gmail.com>
2023-09-04 15:20:35 +0300

    Exclude application.properties and edit AccountController

--- src/main/java/com/bankmanagement/banker/accountController/AccountController.java
+++ src/main/java/com/bankmanagement/banker/accountController/AccountController.java
@@ -1,9 +1,13 @@
 package com.bankmanagement.banker.accountController;
 
+import com.bankmanagement.banker.model.Account;
 import com.bankmanagement.banker.service.AccountService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/account")
@@ -20,4 +24,21 @@ public class AccountController {
     public String hello() {
         return accountService.Hello();
     }
+
+    @GetMapping("/return-all-accounts")
+    public List<Account> returnAllAccounts(){
+        //  TO DO
+        return null;
+    }
+    @PutMapping("/update-account/{id}")
+    public String updateAccount(){
+        // TO DO
+        return "Account details updated successfully";
+    }
+    @GetMapping("/check-account/{id}")
+    public String getAccountDetails(){
+        // TO DO
+        return "Account details fetched successfully";
+    }
+
 }
