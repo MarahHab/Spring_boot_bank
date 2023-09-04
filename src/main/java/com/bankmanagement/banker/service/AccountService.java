5ca9b2e57a4644c18ab5d5bb25688c8f77cedf21
Marah Habashi <marah.hab2@gmail.com>
2023-09-04 14:40:19 +0300

    fake database

--- src/main/java/com/bankmanagement/banker/service/AccountService.java
+++ src/main/java/com/bankmanagement/banker/service/AccountService.java
@@ -1,11 +1,34 @@
 package com.bankmanagement.banker.service;
 
+import com.bankmanagement.banker.model.Account;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AccountService {
 
-    public String Hello() {
-        return "Hello World";
+
+    List<Account> accounts = new ArrayList<Account>();
+
+    public AccountService(List<Account> accounts) {
+        this.accounts = accounts;
+        Account newAccount1 = new Account("256984","Mhmd",100.0);
+        Account newAccount3 = new Account("524874","John",460.0);
+        Account newAccount2 = new Account("125423","Marah",900.0);
+        Account newAccount4 = new Account("956324","Alex",215.2);
+        accounts.add(newAccount1);
+        accounts.add(newAccount2);
+        accounts.add(newAccount3);
+        accounts.add(newAccount4);
     }
+
+    public String Hello(){
+        return accounts.toString();
+    }
+
+
+
 }
