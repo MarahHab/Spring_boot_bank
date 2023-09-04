d6bcaefe46af443c9393b10927a6860f711623e8
mohamedsayedahmd <mohamedsayedahmd@gmail.com>
2023-09-04 22:58:39 +0300

    organize the code

--- src/main/java/com/bankmanagement/banker/service/AccountService.java
+++ src/main/java/com/bankmanagement/banker/service/AccountService.java
@@ -5,9 +5,6 @@ import com.bankmanagement.banker.repo.AccountRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 public class AccountService {
 
@@ -16,28 +13,11 @@ public class AccountService {
     public AccountService(AccountRepo accountRepo) {
         this.accountRepo = accountRepo;
     }
-
-    List<Account> accounts = new ArrayList<Account>();
-
-    public AccountService(AccountRepo accountRepo, List<Account> accounts) {
-        this.accountRepo = accountRepo;
-        this.accounts = accounts;
-        Account newAccount1 = new Account("256984","Mhmd",100.0);
-        Account newAccount3 = new Account("524874","John",460.0);
-        Account newAccount2 = new Account("125423","Marah",900.0);
-        Account newAccount4 = new Account("956324","Alex",215.2);
-        accounts.add(newAccount1);
-        accounts.add(newAccount2);
-        accounts.add(newAccount3);
-        accounts.add(newAccount4);
+    // --> create new account
+    public String createNewAccount(){
+        Account user = new Account("28953", "Marah", 240.30);
+        Account save = accountRepo.save(user);
+        return save.toString();
     }
-
-    public String Hello(){
-        Account mhmd = new Account("256984", "Mhmd", 100.0);
-        accountRepo.save(mhmd);
-        return accounts.toString();
-    }
-
-
 
 }
